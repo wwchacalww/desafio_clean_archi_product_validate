1c2813ea318f8a480f6b36d45331a438c27359bb
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-05-04 12:04:33 -0300

    Desafio Concluído

--- README.md
+++ README.md
@@ -4,9 +4,12 @@
 ### Desafio
 Desafio: 
 
-[  ] Criar o processo de validação na entidade Product.<br />
-[  ] Minimizar o acoplamento em nosso domínio.<br />
-[  ] verificar se todos os testes ainda estão passando.<br />
+[ X ] Criar o processo de validação na entidade Product.<br />
+[ X ] Minimizar o acoplamento em nosso domínio.<br />
+[ X ] verificar se todos os testes ainda estão passando.<br />
 
 ### Solução
 - Crio a pasta e a interface de validação do product /src/domain/product/validator/product.yup.validador.ts
+- Crio o factory que gera o validate na Entity Product /src/domain/product/factory/product.validator.factory.ts
+- Refatoro o metodo validate na Entity Product chamando o factory de validação
+- Efetuo todos os testes
---
+++ src/domain/product/validator/product.yup.validator.ts
@@ -0,0 +1,34 @@
+import ValidatorInterface from "../../@shared/validator/validator.interface";
+import * as yup from "yup";
+import Product from "../entity/product";
+
+export default class ProductYupValidator
+  implements ValidatorInterface<Product>{
+  validate(entity: Product): void {
+    try {
+      yup
+        .object()
+        .shape({
+          id: yup.string().required("Id is required"),
+          name: yup.string().required("Name is required"),
+          price: yup.number().moreThan(0, "Price must be greater than zero").required("Price is required"),
+        })
+        .validateSync({
+          id: entity.id,
+          name: entity.name,
+          price: entity.price,
+        }, {
+          abortEarly: false,
+        })
+    } catch (errors) {
+      const e = errors as yup.ValidationError;
+      e.errors.forEach((error) => {
+        entity.notification.addError({
+          context: "product",
+          message: error,
+        })
+      })
+    }
+  }
+
+}
